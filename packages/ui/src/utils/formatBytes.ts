5b21a2f3afc721d0318f929106a41e377ea824ae
Tomas Heky Hekrla <tomas@heky.eu>
2024-11-25 22:11:12 +0100

    feat: formatBytes

--- packages/ui/src/utils/formatBytes.ts
+++ packages/ui/src/utils/formatBytes.ts
@@ -1,7 +1,12 @@
 export function formatBytes(bytes: number): [number, string] {
-  const k = 1000;
   const units = ['b', 'kb', 'Mb', 'Gb', 'Tb'] as const;
-  const i = Math.floor(Math.log(bytes) / Math.log(k));
+
+  if (bytes === 0) return [0, units[0]];
+
+  const k = 1000;
+
+  const i = Math.floor(Math.log(Math.abs(bytes)) / Math.log(k));
+
   return [
     bytes / Math.pow(k, Math.min(i, units.length - 1)),
     units.at(i) ?? units.at(-1) ?? '',
